f477bfad68348205a988064a6e9342dae67067cc
Entropie67 <olivier.elophe@gmail.com>
2020-01-09 17:05:05 +0100

    début

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// message en console
+console.log("coucou");
+//popup
+//alert("Vous avez été hacké !");
+
+boutton = document.querySelector("#monbouton");
+
+boutton.addEventListener("click",function (){
+    //alert("Click ! click !");
+    document.querySelector("#para").style.color='blue';
+    document.querySelector("h1").style.display="none";
+    document.querySelector("img").style.display="block";
+});
+
+image = document.querySelector("img");
+
+
+
+
