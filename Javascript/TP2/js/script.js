fa38903c6f000f03badaf8df73ac14fcc5d47ec6
Entropie67 <olivier.elophe@gmail.com>
2020-01-17 14:14:26 +0100

    butons actif

--- Javascript/TP2/js/script.js
+++ Javascript/TP2/js/script.js
@@ -1,3 +1,16 @@
 
+var perso1 = 0;
+var perso2 = 0;
+var perso3 = 0;
+function action(){
+    perso1++;
+    personnageID =  this.getAttribute('id')
+    console.log("compteur",);
+    alert("Tu as cliqué sur " + personnageID);
+}
 
-alert("coucou");
+bouttons = document.querySelectorAll(".vote");
+for (let i=0; i<3; i++){
+    bouttons[i].addEventListener("click", action);
+}
+
