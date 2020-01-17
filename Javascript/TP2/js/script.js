0740e5cbbcfa00c2a562f2528c254f1cf64cf23a
Entropie67 <olivier.elophe@gmail.com>
2020-01-17 14:27:53 +0100

    avec footer

--- Javascript/TP2/js/script.js
+++ Javascript/TP2/js/script.js
@@ -3,10 +3,27 @@ var perso1 = 0;
 var perso2 = 0;
 var perso3 = 0;
 function action(){
-    perso1++;
+
     personnageID =  this.getAttribute('id')
-    console.log("compteur",);
-    alert("Tu as cliqué sur " + personnageID);
+    //alert("Tu as cliqué sur " + personnageID);
+    switch (personnageID){
+        case "CM":
+            alert("Tu as choisi Carole");
+            perso1++;
+            break;
+        case "WM":
+            alert("Tu as choisi Wanda");
+            perso2++;
+            break;
+        case "BW":
+            alert("Tu as choisi Natasha");
+            perso3++;
+            break;
+        default:
+            console.log("bug");
+    }
+    console.log("Résultats :",perso1,perso2,perso3);
+
 }
 
 bouttons = document.querySelectorAll(".vote");
